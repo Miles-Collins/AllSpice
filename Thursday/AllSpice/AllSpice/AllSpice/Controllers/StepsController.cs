3cbc2a4a4fffe204b0cb65f8dc97852062c5841b
Miles Collins <miles.collins@Ymail.com>
2022-09-15 19:46:10 -0600

    init

---
+++ Thursday/AllSpice/AllSpice/AllSpice/Controllers/StepsController.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace AllSpice.Controllers
+{
+  [ApiController]
+  [Route("api/[controller]")]
+  public class StepsController : ControllerBase
+  {
+    public StepsController()
+    {
+    }
+  }
+}
